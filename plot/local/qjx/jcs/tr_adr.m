09106a06ccf6ab41dc6299eee4083879a1370ef9
Unknown <hewitt.archie@yandex.ru>
2018-11-13 14:02:07 +0300

    lambda evo plotted

--- plot/local/qjx/jcs/tr_adr.m
+++ plot/local/qjx/jcs/tr_adr.m
@@ -1,6 +1,6 @@
 clear all;
 
-tr_id = 5;
+tr_id = 6;
 
 seed = 1;
 num_seeds = 1000000;
@@ -10,7 +10,7 @@ diss_type = 0;
 diss_gamma = 0.1;
 diss_phase = 0.0;
 
-T_part = 0.05;
+T_part = 2;
 
 drv_T_1 = 0.98 * T_part;
 drv_T_2 = 1.00 * T_part;
@@ -25,6 +25,7 @@ T = drv_T_1 + drv_T_2;
 
 cd_dump_deep = 0;
 cd_num_sub_steps = 10;
+
 
 data_path = '../../../../source/cpp/QJX/QJX';
 
@@ -43,7 +44,7 @@ suffix = sprintf('rnd(%d_%d)_N(%d)_diss(%d_%0.4f_%0.4f)_drv(%0.4f_%0.4f_%0.4f)_p
     start_state);
 
 fn = sprintf('%s/periods_%s.txt', data_path, suffix);
-dump_periods = importdata(fn) - 2;
+dump_periods = importdata(fn);
 num_dumps = size(dump_periods, 1);
 
 if(cd_dump_deep == 1)
@@ -55,22 +56,32 @@ if(cd_dump_deep == 1)
     end
 end
 
+adr = zeros(size_sys, num_dumps);
 
-fn = sprintf('%s/lambda_evo_%s.txt', data_path, suffix);
-evo_data = importdata(fn);
+fn = sprintf('%s/adr_%d_%s.txt', data_path, tr_id, suffix);
+adr_data = importdata(fn);
+
+for dump_id = 1:num_dumps
+    for i = 1:size_sys
+        adr(i, dump_id) = adr_data((dump_id-1)*size_sys + i);
+    end
+end
+
+states = linspace(1, size_sys, size_sys) / size_sys;
 
 
-evo_curr = evo_data(:, tr_id + 1);
-
-hLine = plot(dump_periods, evo_curr);
-legend(hLine, sprintf('tr=%d', tr_id))
+fig = figure;
+hLine = plot(states, adr(:, end));
+title_str = sprintf('$tr=%d$ $N=%d$ $E=%0.2f$ $U=%0.2f$ $J=%0.2f$ ', tr_id, N, prm_E, prm_U, prm_J);
+title(title_str, 'FontSize', 33, 'interpreter','latex');
 set(gca, 'FontSize', 30);
-xlabel('$t/T$', 'Interpreter', 'latex');
-xlim([dump_periods(1) dump_periods(end)])
+xlabel('$n$', 'Interpreter', 'latex');
+xlim([states(1) states(end)])
 set(gca, 'FontSize', 30);
-ylabel('$\lambda$', 'Interpreter', 'latex');
+ylabel('$n$', 'Interpreter', 'latex');
+colormap hot;
 hold all;
 
-propertyeditor('on')
+propertyeditor(fig)
 
 
