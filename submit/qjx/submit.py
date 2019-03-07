443ca039f2789f336b34008c3aad91f98df0f542
Unknown <hewitt.archie@yandex.ru>
2019-03-08 01:53:49 +0300

    Changed python run

--- submit/qjx/submit.py
+++ submit/qjx/submit.py
@@ -33,7 +33,6 @@ for eps_id in range(0, eps_num):
             dimer_U = dimer_U_start + dimer_U_id * dimer_U_shift
 
             for jcs_ampl_id in range(0, jcs_ampl_num):
-
                 jcs_ampl = jcs_ampl_start + jcs_ampl_id * jcs_ampl_shift
 
                 print('eps: ' + str(eps))
@@ -42,7 +41,7 @@ for eps_id in range(0, eps_num):
                 print('jcs_ampl: ' + str(jcs_ampl))
 
                 sys_id = 0
-                task_id = 1
+                task_id = 4
                 prop_id = 1
                 is_debug = 0
                 is_pp = 1
@@ -51,23 +50,24 @@ for eps_id in range(0, eps_num):
                 seed = 0
                 mns = 1000000
                 num_threads = 32
-                num_trajectories = 96
+                num_trajectories = 32
                 num_tp_periods = 100
                 num_obs_periods = 100
                 ex_deep = 16
-                rk_ns = 10000
+                rk_ns = 10
 
                 lpn_type = 0
-                lpn_eps = 1.0e-2
-                lpn_eps_change = 1.25
+                lpn_eps_deep = 10
+                lpn_eps_high = 0
+                lpn_eps_low = -4
                 lpn_delta_s_high = 1.0e-3
                 lpn_delta_s_low = 1.0e-4
                 lpn_delta_f_high = 1.0e-2
                 lpn_delta_f_low = 1.0e-10
                 dump_obs = 1
-                dump_adr_sep = 1
-                dump_adr_avg = 1
-                dump_evo_sep = 0
+                dump_adr_sep = 0
+                dump_adr_avg = 0
+                dump_evo_sep = 1
                 dump_evo_avg = 0
                 dump_type = 0
                 dump_num = 1
@@ -90,8 +90,7 @@ for eps_id in range(0, eps_num):
                 start_state = 0
                 cd_dim = dim
                 cd_eps = eps
-                deep_num_steps = 128
-                deep_dump = 1
+                deep_num_steps = 1000
                 jump = 0
                 jumps_counts = 0
 
@@ -123,7 +122,8 @@ for eps_id in range(0, eps_num):
                         '/run_' + str(ex_deep) + '_' + str(rk_ns) + '_' + str(num_tp_periods) + '_' + str(num_obs_periods) + \
                         '/N_' + str(N) + \
                         '/diss_' + str(diss_type) + '_' + diss_gamma_str + '_' + diss_phase_str + \
-                        '/drv_' + str(dimer_drv_type) + '_' + dimer_drv_ampl_str + '_' + dimer_drv_freq_str + '_' + dimer_drv_phase_str + \
+                        '/drv_' + str(
+                            dimer_drv_type) + '_' + dimer_drv_ampl_str + '_' + dimer_drv_freq_str + '_' + dimer_drv_phase_str + \
                         '/prm_' + dimer_prm_E_str + '_' + dimer_prm_U_str + '_' + dimer_prm_J_str + \
                         '/start_' + str(start_type) + '_' + str(start_state)
 
@@ -144,63 +144,63 @@ for eps_id in range(0, eps_num):
 
                     file_config = open(fn_path + '/config.txt', 'w')
 
-                    file_config.write('sys_id '             + str(sys_id) + '\n')
-                    file_config.write('task_id '            + str(task_id) + '\n')
-                    file_config.write('prop_id '            + str(prop_id) + '\n')
-                    file_config.write('is_debug '           + str(is_debug) + '\n')
-                    file_config.write('is_pp '              + str(is_pp) + '\n')
-                    file_config.write('init_fn '            + init_fn + '\n')
-                    file_config.write('path '               + path + '\n')
-                    file_config.write('seed '               + str(ss) + '\n')
-                    file_config.write('mns '                + str(mns) + '\n')
-                    file_config.write('num_threads '        + str(num_threads) + '\n')
-                    file_config.write('num_trajectories '   + str(num_trajectories) + '\n')
-                    file_config.write('num_tp_periods '     + str(num_tp_periods) + '\n')
-                    file_config.write('num_obs_periods '    + str(num_obs_periods) + '\n')
-                    file_config.write('ex_deep '            + str(ex_deep) + '\n')
-                    file_config.write('rk_ns '              + str(rk_ns))
+                    file_config.write('sys_id ' + str(sys_id) + '\n')
+                    file_config.write('task_id ' + str(task_id) + '\n')
+                    file_config.write('prop_id ' + str(prop_id) + '\n')
+                    file_config.write('is_debug ' + str(is_debug) + '\n')
+                    file_config.write('is_pp ' + str(is_pp) + '\n')
+                    file_config.write('init_fn ' + init_fn + '\n')
+                    file_config.write('path ' + path + '\n')
+                    file_config.write('seed ' + str(ss) + '\n')
+                    file_config.write('mns ' + str(mns) + '\n')
+                    file_config.write('num_threads ' + str(num_threads) + '\n')
+                    file_config.write('num_trajectories ' + str(num_trajectories) + '\n')
+                    file_config.write('num_tp_periods ' + str(num_tp_periods) + '\n')
+                    file_config.write('num_obs_periods ' + str(num_obs_periods) + '\n')
+                    file_config.write('ex_deep ' + str(ex_deep) + '\n')
+                    file_config.write('rk_ns ' + str(rk_ns))
 
                     file_config.close()
 
                     file_params = open(fn_path + '/params.txt', 'w')
 
-                    file_params.write('lpn_type '           + str(lpn_type) + '\n')
-                    file_params.write('lpn_eps '            + str(lpn_eps) + '\n')
-                    file_params.write('lpn_eps_change '     + str(lpn_eps_change) + '\n')
-                    file_params.write('lpn_delta_s_high '   + str(lpn_delta_s_high) + '\n')
-                    file_params.write('lpn_delta_s_low '    + str(lpn_delta_s_low) + '\n')
-                    file_params.write('lpn_delta_f_high '   + str(lpn_delta_f_high) + '\n')
-                    file_params.write('lpn_delta_f_low '    + str(lpn_delta_f_low) + '\n')
-                    file_params.write('dump_obs '           + str(dump_obs) + '\n')
-                    file_params.write('dump_adr_sep '       + str(dump_adr_sep) + '\n')
-                    file_params.write('dump_adr_avg '       + str(dump_adr_avg) + '\n')
-                    file_params.write('dump_evo_sep '       + str(dump_evo_sep) + '\n')
-                    file_params.write('dump_evo_avg '       + str(dump_evo_avg) + '\n')
-                    file_params.write('dump_type '          + str(dump_type) + '\n')
-                    file_params.write('dump_num '           + str(dump_num) + '\n')
-                    file_params.write('N '                  + str(N) + '\n')
-                    file_params.write('diss_type '          + str(diss_type) + '\n')
-                    file_params.write('diss_gamma '         + str(diss_gamma) + '\n')
-                    file_params.write('diss_phase '         + str(diss_phase) + '\n')
-                    file_params.write('dimer_drv_type '     + str(dimer_drv_type) + '\n')
-                    file_params.write('dimer_drv_ampl '     + str(dimer_drv_ampl) + '\n')
-                    file_params.write('dimer_drv_freq '     + str(dimer_drv_freq) + '\n')
-                    file_params.write('dimer_drv_phase '    + str(dimer_drv_phase) + '\n')
-                    file_params.write('dimer_prm_E '        + str(dimer_prm_E) + '\n')
-                    file_params.write('dimer_prm_U '        + str(dimer_prm_U) + '\n')
-                    file_params.write('dimer_prm_J '        + str(dimer_prm_J) + '\n')
-                    file_params.write('jcs_drv_part_1 '     + str(jcs_drv_part_1) + '\n')
-                    file_params.write('jcs_drv_part_2 '     + str(jcs_drv_part_2) + '\n')
-                    file_params.write('jcs_drv_ampl '       + str(jcs_drv_ampl) + '\n')
-                    file_params.write('jcs_prm_alpha '      + str(jcs_prm_alpha) + '\n')
-                    file_params.write('start_type '         + str(start_type) + '\n')
-                    file_params.write('start_state '        + str(start_state) + '\n')
-                    file_params.write('cd_dim '             + str(cd_dim) + '\n')
-                    file_params.write('cd_eps '             + str(cd_eps) + '\n')
-                    file_params.write('deep_num_steps '     + str(deep_num_steps) + '\n')
-                    file_params.write('deep_dump '          + str(deep_dump) + '\n')
-                    file_params.write('jump '               + str(jump) + '\n')
-                    file_params.write('jumps_counts '       + str(jumps_counts))
+                    file_params.write('lpn_type ' + str(lpn_type) + '\n')
+                    file_params.write('lpn_eps_deep ' + str(lpn_eps_deep) + '\n')
+                    file_params.write('lpn_eps_high ' + str(lpn_eps_high) + '\n')
+                    file_params.write('lpn_eps_low ' + str(lpn_eps_low) + '\n')
+                    file_params.write('lpn_delta_s_high ' + str(lpn_delta_s_high) + '\n')
+                    file_params.write('lpn_delta_s_low ' + str(lpn_delta_s_low) + '\n')
+                    file_params.write('lpn_delta_f_high ' + str(lpn_delta_f_high) + '\n')
+                    file_params.write('lpn_delta_f_low ' + str(lpn_delta_f_low) + '\n')
+                    file_params.write('dump_obs ' + str(dump_obs) + '\n')
+                    file_params.write('dump_adr_sep ' + str(dump_adr_sep) + '\n')
+                    file_params.write('dump_adr_avg ' + str(dump_adr_avg) + '\n')
+                    file_params.write('dump_evo_sep ' + str(dump_evo_sep) + '\n')
+                    file_params.write('dump_evo_avg ' + str(dump_evo_avg) + '\n')
+                    file_params.write('dump_type ' + str(dump_type) + '\n')
+                    file_params.write('dump_num ' + str(dump_num) + '\n')
+                    file_params.write('N ' + str(N) + '\n')
+                    file_params.write('diss_type ' + str(diss_type) + '\n')
+                    file_params.write('diss_gamma ' + str(diss_gamma) + '\n')
+                    file_params.write('diss_phase ' + str(diss_phase) + '\n')
+                    file_params.write('dimer_drv_type ' + str(dimer_drv_type) + '\n')
+                    file_params.write('dimer_drv_ampl ' + str(dimer_drv_ampl) + '\n')
+                    file_params.write('dimer_drv_freq ' + str(dimer_drv_freq) + '\n')
+                    file_params.write('dimer_drv_phase ' + str(dimer_drv_phase) + '\n')
+                    file_params.write('dimer_prm_E ' + str(dimer_prm_E) + '\n')
+                    file_params.write('dimer_prm_U ' + str(dimer_prm_U) + '\n')
+                    file_params.write('dimer_prm_J ' + str(dimer_prm_J) + '\n')
+                    file_params.write('jcs_drv_part_1 ' + str(jcs_drv_part_1) + '\n')
+                    file_params.write('jcs_drv_part_2 ' + str(jcs_drv_part_2) + '\n')
+                    file_params.write('jcs_drv_ampl ' + str(jcs_drv_ampl) + '\n')
+                    file_params.write('jcs_prm_alpha ' + str(jcs_prm_alpha) + '\n')
+                    file_params.write('start_type ' + str(start_type) + '\n')
+                    file_params.write('start_state ' + str(start_state) + '\n')
+                    file_params.write('cd_dim ' + str(cd_dim) + '\n')
+                    file_params.write('cd_eps ' + str(cd_eps) + '\n')
+                    file_params.write('deep_num_steps ' + str(deep_num_steps) + '\n')
+                    file_params.write('jump ' + str(jump) + '\n')
+                    file_params.write('jumps_counts ' + str(jumps_counts))
 
                     file_params.close()
 
@@ -211,9 +211,10 @@ for eps_id in range(0, eps_num):
                             'config(' + str(sys_id) + '_' + str(task_id) + '_' + str(prop_id) + ')_' + \
                             'rnd(' + str(ss) + '_' + str(mns) + ')_' + \
                             'N(' + str(N) + ')_' + \
-                            'diss(' + str(diss_type) + '_' + diss_gamma_str + '_' + diss_phase_str+')_' + \
-                            'drv(' + str(dimer_drv_type) + '_' + dimer_drv_ampl_str + '_' + dimer_drv_freq_str + '_' + dimer_drv_phase_str + ')_' + \
-                            'prm(' + dimer_prm_E_str + '_' + dimer_prm_U_str + '_' + dimer_prm_J_str +')_' + \
+                            'diss(' + str(diss_type) + '_' + diss_gamma_str + '_' + diss_phase_str + ')_' + \
+                            'drv(' + str(
+                                dimer_drv_type) + '_' + dimer_drv_ampl_str + '_' + dimer_drv_freq_str + '_' + dimer_drv_phase_str + ')_' + \
+                            'prm(' + dimer_prm_E_str + '_' + dimer_prm_U_str + '_' + dimer_prm_J_str + ')_' + \
                             'start(' + str(start_type) + '_' + str(start_state) + ')'
 
                     elif sys_id == 1:
@@ -234,7 +235,6 @@ for eps_id in range(0, eps_num):
                         fn_test = fn_path + '/spec_' + fn_suffix + '.txt'
 
                     if not os.path.isfile(fn_test):
-					
                         os.system('sbatch run_unn.sh ' + fn_path)
 
 
