2b0d26e5ffb89b0a27defb4a24a6be712b8d7dda
Archie Hewitt <hewitt.archie@yandex.ru>
2019-09-15 17:08:37 +0300

    mbl checking

--- source/cpp/QJX/QJX/experiment.cpp
+++ source/cpp/QJX/QJX/experiment.cpp
@@ -258,7 +258,7 @@ void LpnMultExperimentBehaviour::obser_process(AllData * ad, PropagateBehavior *
 
 		if (dump_evo_avg == 1)
 		{
-			dump_adr_avg(ad, true);
+			dump_adr_avg_mult(ad, true, 0, num_trajectories / 2);
 		}
 	}
 
@@ -274,6 +274,7 @@ void LpnMultExperimentBehaviour::obser_process(AllData * ad, PropagateBehavior *
 	if (dump_evo_avg == 0)
 	{
 		dump_adr_avg(ad, true);
+		dump_adr_avg_mult(ad, true, 0, num_trajectories / 2);
 	}
 }
 
