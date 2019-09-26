18340e1632d4b060c57013b362889a81b20dc3a2
Unknown <hewitt.archie@yandex.ru>
2019-09-26 14:17:50 +0300

    lpn checking local

--- plot/local/qjx/mbl/tr_mean_diff_evo.m
+++ plot/local/qjx/mbl/tr_mean_diff_evo.m
@@ -1,39 +1,45 @@
 clear all;
 
-seed = 1;
+seed = 0;
 mns = 1000000;
 
 Nc = 8;
+
+W_seed = 1;
+W_mns = 1000000;
 
 diss_type = 1;
 diss_phase = 0.0;
 diss_gamma = 0.1;
 
-prm_W = 1.0;
+prm_W = 20;
 prm_U = 1.0;
 prm_J = 1.0;
 
 start_type = 0;
 start_state = 49;
 
-T = 1;
+delta_lpn_s = 1e-5;
+delta_lpn_f = 0.1;
 
 deep_dump = 0;
 deep_num_steps = 100;
 
-num_plot_traj = 50;
+num_plot_traj = 5;
 
 fig = figure;
 
-num_trajectories = 50;
+num_trajectories = 5;
 
 
 data_path = '../../../../source/cpp/QJX/QJX';
 
-suffix = sprintf('rnd(%d_%d)_Nc(%d)_diss(%d_%0.4f_%0.4f)_prm(%0.4f_%0.4f_%0.4f)_start(%d_%d)', ...
+suffix = sprintf('rnd(%d_%d)_Nc(%d)_rnd(%d_%d)_diss(%d_%0.4f_%0.4f)_prm(%0.4f_%0.4f_%0.4f)_start(%d_%d)_lpn(%0.4f_%0.4f)', ...
     seed, ...
     mns, ...
     Nc, ...
+    W_seed, ...
+    W_mns, ...
     diss_type, ...
     diss_phase, ...
     diss_gamma, ...
@@ -41,7 +47,9 @@ suffix = sprintf('rnd(%d_%d)_Nc(%d)_diss(%d_%0.4f_%0.4f)_prm(%0.4f_%0.4f_%0.4f)_
     prm_U, ...
     prm_J, ...
     start_type, ...
-    start_state);
+    start_state, ...
+    log10(delta_lpn_s), ...
+    log10(delta_lpn_f));
 
 fn = sprintf('%s/periods_%s.txt', data_path, suffix);
 dump_periods = importdata(fn);
@@ -56,7 +64,7 @@ if(deep_dump == 1)
     end
 end
 
-fn = sprintf('%s/spec_lpn_evo_%s.txt', data_path, suffix);
+fn = sprintf('%s/spec_evo_%s.txt', data_path, suffix);
 spec_evo_data = importdata(fn);
 
 for tr_id = 1 : num_plot_traj
@@ -78,6 +86,11 @@ for tr_id = 1 : num_plot_traj
     ylim([1e-12, 10]);
     
 end
+
+set(gca, 'YScale', 'log')
+ylim([1e-12, 10]);
+plot([dump_periods(1) dump_periods(end)], [delta_lpn_f delta_lpn_f], 'LineStyle', '--', 'color', 'black');
+plot([dump_periods(1) dump_periods(end)], [delta_lpn_s delta_lpn_s], 'LineStyle', '--', 'color', 'black');
 
 fn = sprintf('%s/lambda_%s.txt', data_path, suffix);
 lambda_data = importdata(fn);
