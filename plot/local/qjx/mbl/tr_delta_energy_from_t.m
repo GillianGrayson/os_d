76eb0e8dce1ebdc496665aca411ea29a7f58a368
Archie Hewitt <hewitt.archie@yandex.ru>
2020-03-27 17:30:52 +0300

    mbl deep checking

--- plot/local/qjx/mbl/tr_delta_energy_from_t.m
+++ plot/local/qjx/mbl/tr_delta_energy_from_t.m
@@ -1,8 +1,12 @@
 clear all;
 
+T = 0.1;
+
 sys_id = 3;
-task_id = 7;
+task_id = 8;
 prop_id = 0;
+
+is_jump = 1;
 
 seed = 1;
 mns = 1000000;
@@ -16,26 +20,27 @@ diss_type = 1;
 diss_phase = 0.0;
 diss_gamma = 0.1;
 
-prm_W = 2.0;
+prm_W = 20.0;
 prm_U = 1.0;
 prm_J = 1.0;
 
 start_type = 0;
-start_state = 49;
+start_state = 0;
 
-delta_lpn_s = 1e-5;
-delta_lpn_f_h = 1000000;
-delta_lpn_f_l = 1e-12;
+lpn_type = -1;
+delta_lpn_s = 1e-6;
+delta_lpn_f_h = 10;
+delta_lpn_f_l = 1e-16;
 
-deep_dump = 0;
-deep_num_steps = 1;
+deep_dump = 1;
+deep_num_steps = 100;
 
 num_target_traj = 10;
 num_plot_traj = 10;
 
 data_path = '../../../../source/cpp/QJX/QJX';
 
-suffix = sprintf('setup(%d_%d_%d)_rnd(%d_%d)_Nc(%d)_rnd(%d_%d)_diss(%d_%0.4f_%0.4f)_prm(%0.4f_%0.4f_%0.4f)_start(%d_%d)_lpn(%d_%0.4f_%0.4f)', ...
+suffix = sprintf('setup(%d_%d_%d)_rnd(%d_%d)_Nc(%d)_rnd(%d_%d)_diss(%d_%0.4f_%0.4f)_prm(%0.4f_%0.4f_%0.4f)_start(%d_%d)_lpn(%d_%0.4f_%0.4f_%0.4f)', ...
     sys_id, ...
     task_id, ...
     prop_id, ...
@@ -52,9 +57,10 @@ suffix = sprintf('setup(%d_%d_%d)_rnd(%d_%d)_Nc(%d)_rnd(%d_%d)_diss(%d_%0.4f_%0.
     prm_J, ...
     start_type, ...
     start_state, ...
-    0, ...
+    lpn_type, ...
     log10(delta_lpn_s), ...
-    log10(delta_lpn_f_h));
+    log10(delta_lpn_f_h), ...
+    log10(delta_lpn_f_l));
 
 fn = sprintf('%s/periods_%s.txt', data_path, suffix);
 dump_periods = importdata(fn);
@@ -68,6 +74,8 @@ if(deep_dump == 1)
         dump_periods(dump_id) = dump_shift * (dump_id - 1);
     end
 end
+
+dump_periods = dump_periods * T;
 
 fn = sprintf('%s/spec_lpn_evo_%s.txt', data_path, suffix);
 spec_evo_data = importdata(fn);
@@ -93,6 +101,23 @@ for tr_id = 1 : num_plot_traj
     hold all;
     set(gca, 'YScale', 'log')
     ylim([1e-12, 10]);
+    
+    
+    if (is_jump == 1)
+        color = get(hLine,'Color');
+        
+        fn = sprintf('%s/jump_times_%d_%s.txt', data_path, (tr_id - 1), suffix);
+        jump_data_1 = importdata(fn);
+        for j_id = 1:size(jump_data_1, 1)
+            plot([jump_data_1(j_id) jump_data_1(j_id)], [1e-20 1e+1], 'Color', color)
+        end
+        
+        fn = sprintf('%s/jump_times_%d_%s.txt', data_path, (tr_id - 1 + num_target_traj), suffix);
+        jump_data_2 = importdata(fn);
+        for j_id = 1:size(jump_data_2, 1)
+            plot([jump_data_2(j_id) jump_data_2(j_id)], [1e-20 1e+1], 'Color', color)
+        end
+    end
     
 end
 
